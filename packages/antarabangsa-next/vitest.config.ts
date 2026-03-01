d00b88c318616267f56b365949f1388c1e13dcdd
Syafiq Basri <altsyafiq@gmail.com>
2026-03-01 23:12:35 +0800

    feat: better improve test

--- packages/antarabangsa-next/vitest.config.ts
+++ packages/antarabangsa-next/vitest.config.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
@@ -11,6 +12,7 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			'@': '/src',
+			'@lanun/antarabangsa': path.resolve(__dirname, '../antarabangsa/src'),
 		},
 	},
 })
