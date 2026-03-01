edf0163998676fd913c60dafcda726f7a7d72223
Syafiq Basri <altsyafiq@gmail.com>
2026-03-01 22:58:30 +0800

    chore: updated lint

--- packages/antarabangsa-next/src/types.ts
+++ packages/antarabangsa-next/src/types.ts
@@ -2,11 +2,11 @@
  * Configuration options for the locale middleware factory.
  */
 export type MiddlewareOptions = {
-  /**
-   * List of pathname routes that should never be locale prefixed.
-   * Values must be provided as absolute pathnames starting with `/`.
-   *
-   * @defaultValue []
-   */
-  noLocaleRoutes?: string[];
-};
+	/**
+	 * List of pathname routes that should never be locale prefixed.
+	 * Values must be provided as absolute pathnames starting with `/`.
+	 *
+	 * @defaultValue []
+	 */
+	noLocaleRoutes?: string[]
+}
