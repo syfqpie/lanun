8702b8be1a5c7f93230995651afbe6baaa512a6a
Syafiq Basri <altsyafiq@gmail.com>
2026-03-05 01:38:46 +0800

    feat: added translation configs

--- apps/antarabangsa-docs/src/lib/i18n.ts
+++ apps/antarabangsa-docs/src/lib/i18n.ts
@@ -1,7 +1,22 @@
+import { getTranslation as getTranslationFn } from '@lanun/antarabangsa'
+import type { Locale, TranslationConfig } from '@lanun/antarabangsa'
 import { defineI18n } from 'fumadocs-core/i18n'
+
+import enTrx from '@/locales/en.json'
 
 export const i18n = defineI18n({
 	defaultLanguage: 'en',
 	languages: ['en', 'ms'],
 	hideLocale: 'default-locale',
 })
+
+export const translations: TranslationConfig = {
+	defaultLocale: 'en',
+	translations: {
+		en: enTrx,
+	},
+}
+
+export const getTranslation = (locale: Locale) => {
+	return getTranslationFn(locale, translations)
+}
