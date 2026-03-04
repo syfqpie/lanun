443fe9bf1f2a0a733b388ee7f2c4d5907385a159
Syafiq Basri <altsyafiq@gmail.com>
2026-03-05 00:18:39 +0800

    feat: added AppLogo

---
+++ apps/antarabangsa-docs/src/components/common/app-logo.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { FC, useEffect, useState } from 'react'
+import Image from 'next/image'
+import { useTheme } from 'next-themes'
+import type { ImageProps } from 'next/image'
+
+import { cn } from '@/lib/cn'
+
+type AppLogoProps = Omit<ImageProps, 'src' | 'alt'>
+
+const LOGO_DARK_MODE = '/logo/flag-white.png'
+const LOGO_LIGHT_MODE = '/logo/flag.png'
+
+export const AppLogo: FC<AppLogoProps> = ({
+	className,
+	height = 2000,
+	width = 2000,
+	...rest
+}) => {
+	const [mounted, setMounted] = useState(false)
+	const { resolvedTheme } = useTheme()
+
+	useEffect(() => {
+		setMounted(true)
+	}, [])
+
+	if (!mounted) return null
+
+	const src = resolvedTheme === 'light' ? LOGO_LIGHT_MODE : LOGO_DARK_MODE
+
+	return (
+		<Image
+			suppressHydrationWarning
+			height={height}
+			width={width}
+			className={cn('w-60 mx-auto self-center center', className)}
+			{...rest}
+			alt="@lanun/antarabangsa logo"
+			aria-label="@lanun/antarabangsa logo"
+			src={src}
+		/>
+	)
+}
